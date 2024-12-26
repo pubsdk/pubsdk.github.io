d280c93b402a0bb5ded281c48fba74126fc609c5
taugin <taugin@163.com>
2024-12-26 16:09:44 +0800

    1. upload maven files

---
+++ config/file_list.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+echo "SCRIPT_DIR = $SCRIPT_DIR"
+# 定义 JSON 文件的路径
+JSON_FILE="$SCRIPT_DIR/file_list.json"
+echo "JSON_FILE = $JSON_FILE"
+
+# 获取所有被跟踪的文件
+#FILES=$(git ls-files)
+RELEASE="release/"
+SNAPSHOT="snapshot/"
+FILES=$(find $RELEASE $SNAPSHOT -type f)
+
+# 开始创建 JSON 文件
+echo "[" > $JSON_FILE
+
+# 遍历所有文件并写入 JSON
+FIRST=true
+for FILE in $FILES; do
+  if [ "$FIRST" = true ]; then
+    FIRST=false
+  else
+    echo "," >> $JSON_FILE
+  fi
+
+  # 获取文件的大小和修改时间
+  FILE_SIZE=$(stat -c%s "$FILE" | numfmt --to=iec --format="%.2f" --suffix="B")
+  FILE_MOD_TIME=$(stat -c%y "$FILE")
+  FILE_MOD_TIME=$(date -d "$FILE_MOD_TIME" +"%Y-%m-%d %H:%M:%S")
+
+  echo "  { \"path\": \"$FILE\", \"size\": \"$FILE_SIZE\", \"modified_time\": \"$FILE_MOD_TIME\" }" >> $JSON_FILE
+done
+
+# 结束 JSON 文件
+echo "]" >> $JSON_FILE
+
+# 输出生成的 JSON 文件
+echo "Generated JSON file: $JSON_FILE"
+
+# 允许提交继续
+exit 0
---
+++ upload.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Upload script start to execute ..."
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+$SCRIPT_DIR/config/file_list.sh
+
+WORKDIR=$(pwd)
+
+echo "WORKDIR = $WORKDIR"
+
+REMOTE_URL=$(git remote get-url origin)
+
+echo "REMOTE_URL = $REMOTE_URL"
+
+echo "WEB_PAGE = https://mipsdk.github.io"
+
+BRANCH=$(git branch)
+
+echo "BRANCH = $BRANCH"
+
+git add .
+
+git commit -m "1. upload maven files"
+
+echo "push $BRANCH -> $REMOTE_URL"
+
+git push -u origin master
+
+echo "Upload script execute over ..."
